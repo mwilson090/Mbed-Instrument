ee7e0561d329d7ead81695e89fec323b9bde57cd
mwilson090 <mwilson316@gatech.edu>
2021-12-04 14:29:59 -0500

    plays when button pushed, still display color when resting, fixed muffled sound, only changes period when note changes

--- main.cpp
+++ main.cpp
@@ -12,138 +12,194 @@ Serial pc(USBTX,USBRX); //not used in this program
 
 PwmOut speaker(p21);
 
-double Song[100];
+char Note;
 
+int ind;
+
+char Song[50];
+
+double noteRange = 0.1538;
 double highC = -0.923;
-double B = highC + 0.0769;
-double bFlat = B + 0.0769;
-double A = bFlat + 0.0769;
-double aFlat = A + 0.0769;
-double G = aFlat + 0.0769;
-double gFlat = G + 0.0769;
-double F = gFlat + 0.0769;
-double E = F + 0.0769;
-double eFlat = E + 0.0769;
-double D = eFlat + 0.0769;
-double dFlat = D + 0.0769;
-double C = dFlat + 0.0760;
+double B = highC + noteRange;
+double bFlat = B + noteRange;
+double A = bFlat + noteRange;
+double aFlat = A + noteRange;
+double G = aFlat + noteRange;
+double gFlat = G + noteRange;
+double F = gFlat + noteRange;
+double E = F + noteRange;
+double eFlat = E + noteRange;
+double D = eFlat + noteRange;
+double dFlat = D + noteRange;
+double C = dFlat + noteRange;
 
 double x = 0, y = 0, z = 0;
 
-void rest(){
-    while(pb){
-        speaker = 0.0; //turn off speaker for rest
+void setNote(){
+    if(x < highC){
+        red =1.0;
+        green =1.0;
+        blue =1.0;
+        Note = 'h'; 
+      } 
+      else if(x < B){
+        red =1.0;
+        green =0.0;
+        blue =0.0;
+        Note = 'B';
+      }
+      else if(x < bFlat){
+        red =0.0;
+        green =1.0;
+        blue =0.5;
+        Note = 'b'; 
+      }
+      else if(x < A){ 
+        red =1.0;
+        green =1.0;
+        blue =0.0;
+        Note = 'A'; 
+      }
+      else if(x < aFlat){
+        red =0.5;
+        green =0.0;
+        blue =1.0;
+        Note = 'a'; 
+      }
+      else if(x < G){
+        red =0.0;
+        green =1.0;
+        blue =0.0;
+        Note = 'G'; 
+      }
+      else if(x < gFlat){
+        red =1.0;
+        green =0.5;
+        blue =0.5;
+        Note = 'g'; 
+      }
+      else if(x < F){
+        red =0.0;
+        green =1.0;
+        blue =1.0;
+        Note = 'F'; 
+      }
+      else if(x < E){ 
+        red =0.5;
+        green =1.0;
+        blue =0.0;
+        Note = 'E'; 
+      }
+      else if(x < eFlat){
+        red =1.0;
+        green =0.0;
+        blue =1.0;
+        Note = 'e'; 
+      }
+      else if(x < D){ 
+        red =0.5;
+        green =0.5;
+        blue =1.0;
+        Note = 'D'; 
+      }
+      else if(x < dFlat){ 
+        red =1.0;
+        green =0.0;
+        blue =0.5;
+        Note = 'd'; 
+      }
+      else if(x < C){
+        red =0.0;
+        green =0.0;
+        blue =1.0;
+        Note = 'C'; 
+      }
+      else {
+        red =0.0;
+        green =0.0;
+        blue =0.0;
+        Note = 'r'; 
+      }
+      
+      //override note- rest if y < -0.5
+      if(y < -0.5){
         red = 0.0;
         green = 0.0;
         blue = 0.0;
+        Note = 'r'; 
+      }
+}
+
+void playNote(){
+    //set period based on note- only if the note is different
+    if(ind==0||(ind>0 && Note != Song[ind-1])){
+        switch(Note){
+            case 'A':
+                speaker.period(1.0/880); 
+                break;
+            case 'a':
+                speaker.period(1.0/831); 
+                break;
+            case 'B':
+                speaker.period(1.0/988); 
+                break;
+            case 'b':
+                speaker.period(1.0/932); 
+                break;
+            case 'C':
+                speaker.period(1.0/523); 
+                break;
+            case 'c':
+                break;
+            case 'D':
+                speaker.period(1.0/587); 
+                break;
+            case 'd':
+                speaker.period(1.0/554); 
+                break;
+            case 'E':
+                speaker.period(1.0/659);
+                break;
+            case 'e':
+                speaker.period(1.0/622); 
+                break;
+            case 'F':
+                speaker.period(1.0/698); 
+                break;
+            case 'G':
+                speaker.period(1.0/784); 
+                break;
+            case 'g':
+                speaker.period(1.0/740);
+                break;
+            case 'h':
+                speaker.period(1.0/1047); 
+                break;
+            case 'r':
+                speaker = 0.0;
+                break;
+        }
     }
+
+    //set volume based on y-axis position
+    if(y < -0.5){
+      speaker = 0.0;
+    }
+    else if(y > 0){
+      speaker = y/2; //volume range ~0-50% duty cycle
+    }
+        
 }
 
-void setNote(int i){
-    if(x < highC){
-        speaker.period(1.0/1047); 
-        red =1.0;
-        green =1.0;
-        blue =1.0; 
-      } 
-      else if(x < B){
-        speaker.period(1.0/988); 
-        red =1.0;
-        green =0.0;
-        blue =0.0;
-      }
-      else if(x < bFlat){
-        speaker.period(1.0/932); 
-        red =0.0;
-        green =1.0;
-        blue =0.5;
-      }
-      else if(x < A){
-        speaker.period(1.0/880); 
-        red =1.0;
-        green =1.0;
-        blue =0.0;
-      }
-      else if(x < aFlat){
-        speaker.period(1.0/831); 
-        red =0.5;
-        green =0.0;
-        blue =1.0;
-      }
-      else if(x < G){
-        speaker.period(1.0/784); 
-        red =0.0;
-        green =1.0;
-        blue =0.0;
-      }
-      else if(x < gFlat){
-        speaker.period(1.0/740); 
-        red =1.0;
-        green =0.5;
-        blue =0.5;
-      }
-      else if(x < F){
-        speaker.period(1.0/698); 
-        red =0.0;
-        green =1.0;
-        blue =1.0;
-      }
-      else if(x < E){
-        speaker.period(1.0/659); 
-        red =0.5;
-        green =1.0;
-        blue =0.0;
-      }
-      else if(x < eFlat){
-        speaker.period(1.0/622); 
-        red =1.0;
-        green =0.0;
-        blue =1.0;
-      }
-      else if(x < D){
-        speaker.period(1.0/587); 
-        red =0.5;
-        green =0.5;
-        blue =1.0;
-      }
-      else if(x < dFlat){
-        speaker.period(1.0/554); 
-        red =1.0;
-        green =0.0;
-        blue =0.5;
-      }
-      else if(x < C){
-        speaker.period(1.0/523); 
-        red =0.0;
-        green =0.0;
-        blue =1.0;
-      }
-      else {
-        red =0.0;
-        green =0.0;
-        blue =0.0;
-        speaker = 0.0;
-      }
-      
-      //set volume based on y-axis position
-      if(y < 0){
-        speaker = 0.0;
-        red =0.0;
-        green =0.0;
-        blue =0.0;
-      }
-      else {
-        speaker = y/2; //volume range ~0-50% duty cycle
-      }
-      Song[i] = x;
+void recordNote(){
+    if(ind==0||(ind>0 && Note != Song[ind-1])){
+        Song[ind] = Note;
+        ind++;
+    } 
 }
-
+            
 int main() {
    pb.mode(PullUp);
-   
-   pb.attach_asserted(&rest); //rest when button pushed
-
-   pb.setSampleFrequency(); 
  
    MMA8452 acc(p9, p10, 40000);  //instantiate an acc object! 
    
@@ -152,33 +208,47 @@ int main() {
    acc.setDynamicRange(MMA8452::DYNAMIC_RANGE_4G);
    acc.setDataRate(MMA8452::RATE_100);
    
-   int i;
-   while(i < 100) {
-      
-      //uLCD.circle(-1*y*factor+offsety, -1*x*factor+offsetx, radius, BLACK);
+   while(1){
+       ind = 0;
+       memset(Song, 0, sizeof(Song)); //reset Song array
        
-      if(!acc.isXYZReady()) 
-      {
-         wait(0.01);
-      }
-      else
-      { 
-      
-      acc.readXYZGravity(&x,&y,&z); //notice this is passed by reference use pointers
-      
-      setNote(i);
-      
-      i++;
-      
-      pc.printf("%f, %f, %f\n\n", x, y, z);
-      
-      wait(1);
-      
-      //uLCD.circle(-1*y*factor+offsety, -1*x*factor+offsetx, radius, WHITE);
-      
-      } //end else 
-   } //end infinite while loop
-   for(int i=0; i<100; i++){
-        pc.printf("Note %f: %f\n\n", i, Song[i]);
+       while(ind < 50) {
+          
+          //uLCD.circle(-1*y*factor+offsety, -1*x*factor+offsetx, radius, BLACK);
+           
+          if(!acc.isXYZReady()) 
+          {
+             wait(0.01);
+          }
+          else
+          { 
+          
+          acc.readXYZGravity(&x,&y,&z); //notice this is passed by reference use pointers
+          
+          setNote(); //find note based on position and show it on led
+          
+          //if button pressed, play note
+          if(!pb){
+            playNote();
+          }
+          else {
+              Note = 'r';
+              speaker = 0.0;
+          }
+          
+          recordNote();
+          
+          wait(0.1); //0.01 was giving a very bad sound!
+          
+          } //end else 
+       } //end infinite while loop
+
+       //print final song!
+       speaker = 0.0;
+       red = 1.0;
+       green = 1.0;
+       blue = 1.0;
+       pc.printf("%s\r\n", Song);
+       wait(3);
     }
 } // end main
