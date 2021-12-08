bfdbb2ef01daf147de7f1d934d720a974adf6854
mwilson090 <mwilson316@gatech.edu>
2021-12-08 14:45:21 -0500

    updates

--- README.md
+++ README.md
@@ -1,17 +1,13 @@
 <h1>mbed-instrument</h1>
 
-<p>The mbed instrument plays music based on motion. It connects to wifi, displaying the song play on a webpage. webpage that outputs the song played.</p>
+<p>The mbed instrument plays music based on motion. It connects to wifi, displaying the song played on a webpage. </p>
 
-<h2>How to Play</h2>
-<p>-manuever the board </p>
-<p>-x-axis: dictates notes</p>
-<p>-y-axis: dictates volume</p>
-<p>-button: press to play / release to rest</p>
+<img src= "https://github.gatech.edu/storage/user/20775/files/572f57d0-301e-480f-99d9-6740fc22419a" />
 
 <h2>Components</h2>
 <ul>
   <li>Mbed</li>
-  <li>Push Button</li>
+  <li>Push Button (Uses Internal Pullup)</li>
   <li>RGB Led</li>
   <li>MMA8452 Accelerometer</li>
   <li>Class D High-Efficiency Audio Amp</li>
@@ -25,6 +21,44 @@
 <table>
   <tr>
     <th>Mbed</th>
+    <th>RGB Led</th>
+  </tr>
+  <tr>
+    <td>p25</td>
+    <td>red</td>
+  </tr>
+  <tr>
+    <td>p24</td>
+    <td>green</td>
+  </tr>
+  <tr>
+    <td>p23</td>
+    <td>blue</td>
+  </tr>
+  <tr>
+    <td>gnd</td>
+    <td>gnd</td>
+  </tr>
+</table>
+
+<table>
+  <tr>
+    <th>Mbed</th>
+    <th>Push Button</th>
+  </tr>
+  <tr>
+    <td>p21</td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>gnd</td>
+    <td>gnd</td>
+  </tr>
+</table>
+
+<table>
+  <tr>
+    <th>Mbed</th>
     <th>MMA8452 Accelerometer</th>
   </tr>
   <tr>
@@ -32,16 +66,49 @@
     <td>3.3V</td>
   </tr>
   <tr>
-    <td>p9</td>
+    <td>p27</td>
     <td>sda</td>
   </tr>
   <tr>
-    <td>p10</td>
+    <td>p28</td>
     <td>scl</td>
   </tr>
   <tr>
     <td>gnd</td>
     <td>gnd</td>
+  </tr>
+</table>
+
+<table>
+  <tr>
+    <th>Huzzah</th>
+    <th>Mbed</th>
+    <th>External 5V Supply</th>
+  </tr>
+  <tr>
+    <td>gnd</td>
+    <td>gnd</td>
+    <td>gnd</td>
+  </tr>
+  <tr>
+    <td>tx</td>
+    <td>rx- p9</td>
+    <td></td>
+  </tr>
+  <tr>
+    <td>rx</td>
+    <td>tx- p10</td>
+    <td></td>
+  </tr>
+    <tr>
+    <td>v+</td>
+    <td></td>
+    <td>5V DC</td>
+  </tr>
+    <tr>
+    <td>rest</td>
+    <td>p26 (Optional)</td>
+    <td></td>
   </tr>
 </table>
 
@@ -84,48 +151,30 @@
   </tr>
 </table>
 
-<table>
-  <tr>
-    <th>Huzzah</th>
-    <th>Mbed</th>
-    <th>External 5V Supply</th>
-  </tr>
-  <tr>
-    <td>gnd</td>
-    <td>gnd</td>
-    <td>gnd</td>
-  </tr>
-  <tr>
-    <td>tx</td>
-    <td>rx- p27</td>
-    <td></td>
-  </tr>
-  <tr>
-    <td>rx</td>
-    <td>tx- p28</td>
-    <td></td>
-  </tr>
-    <tr>
-    <td>v+</td>
-    <td></td>
-    <td>5V DC</td>
-  </tr>
-    <tr>
-    <td>rest</td>
-    <td>p26 (Optional)</td>
-    <td></td>
-  </tr>
-</table>
-
 <h2>Set Up</h2>
 <ol>
   <li>Complete wiring connections.</li>
   <li>Upload code to the mbed compiler. Change "ssid" and "password" to your wifi name and password and compile code to the mbed.</li>
+  <img src="https://github.gatech.edu/storage/user/20775/files/e191742f-7de6-4452-bab9-6ea9a929b0f0" />
   <li>Make sure your computer is connected to the same wifi network in the code. Open TeraTerm and connect to your mbed. Press mbed reset button.</li>
-  <li>Go to the IP address outputed on TeraTerm.</li>
-  <li>Select start on the webpage and click submit. Now you can play the instrument!</li>
-  <li>Select stop on the webpage and click submit to see the song you just played.</li>
+  <img src="https://github.gatech.edu/storage/user/20984/files/baa75624-a21d-4d0c-b9bd-4eae42bca201">
+  <img src="https://github.gatech.edu/storage/user/20984/files/04ef696f-6e20-4f89-9d34-3680965225ac">
+  <img src="https://github.gatech.edu/storage/user/20984/files/5cab3885-3dd8-4c32-91a3-49c6715b0c05">
+  <img src="https://github.gatech.edu/storage/user/20984/files/d71921ff-f4f9-4d0f-8014-a848250821c2">
+  <li>Once your terminal prints "Ready" go to the IP address outputed. </li>
+  <img src="https://github.gatech.edu/storage/user/20775/files/e579036b-d6d4-40c4-adfb-c7b530fd235b" />
+  <li>Select start on the webpage and click refresh. Now you can play the instrument!</li>
+  <li>Select stop on the webpage and click refresh to stop playing.</li>
 </ol>
 
+
+<h2>How to Play</h2>
+<p>-manuever the board </p>
+<p>-x-axis: dictates notes</p>
+<p>-y-axis: dictates volume</p>
+<p>-button: press to play / release to rest</p>
+
 <h2>Video Demo</h2>
-<p>Add!</p>
+https://youtu.be/kpXA_2L8fKo
+
+
--- main.cpp
+++ main.cpp
@@ -7,10 +7,41 @@ PwmOut green(p24);
 PwmOut blue(p23);
 
 PinDetect pb(p22);
- 
+
 Serial pc(USBTX,USBRX); //not used in this program
 
+MMA8452 acc(p28, p27, 100000);  //instantiate an acc object! 
+
+//Internet stuff
+Serial esp(p9, p10); // ESP tx, rx
+
+DigitalOut reset(p26);// ESP Reset
+DigitalOut led1(LED1);//Visual aid
+DigitalOut led2(LED2);//Visual aid
+PwmOut led3(LED3); // Visual representation of Volume
+DigitalOut led4(LED4);//Visual aid
+
+Timer t; //Timer for ESP timeouts
+
 PwmOut speaker(p21);
+
+
+//Variables
+int  bytesRead=0;
+int ended;
+int timeout; //ESP related
+char buf[2024];     //ESP input buffer
+char vol[4];        //Volume control
+float vol_f;
+char snd[1024];     //CMD buffer for ESP
+char* num;      // strstr return pointer
+
+bool MUSIC_DEVICE_ENABLED = false;
+
+char ssid[32] = "WyattiPhone";     // WiFi ssid
+char pwd [32] = "yjspsubscale";    // WiFi password
+
+void SendCMD(),getreply(),ESPconfig(); //Function definitions
 
 char Note;
 
@@ -149,8 +180,6 @@ void playNote(){
             case 'C':
                 speaker.period(1.0/523); 
                 break;
-            case 'c':
-                break;
             case 'D':
                 speaker.period(1.0/587); 
                 break;
@@ -178,6 +207,9 @@ void playNote(){
             case 'r':
                 speaker = 0.0;
                 break;
+            default:
+                speaker = 0.0;
+                break;
         }
     }
 
@@ -197,58 +229,346 @@ void recordNote(){
         ind++;
     } 
 }
+
+void ESPconfig()
+{
+
+    wait(5);
+    pc.printf("\f---------- Starting ESP Config ----------\r\n\n");
+    strcpy(snd,".\r\n.\r\n");
+    SendCMD();
+    wait(1);
+    pc.printf("---------- Reset & get Firmware ----------\r\n");
+    strcpy(snd,"node.restart()\r\n");
+    SendCMD();
+    timeout=5;
+    getreply();
+    pc.printf(buf);
+
+    wait(2);
+
+    pc.printf("\n---------- Get Version ----------\r\n");
+    strcpy(snd,"print(node.info())\r\n");
+    SendCMD();
+    timeout=4;
+    getreply();
+    pc.printf(buf);
+
+    wait(3);
+
+    // set CWMODE to 1=Station,2=AP,3=BOTH, default mode 1 (Station)
+    pc.printf("\n---------- Setting Mode ----------\r\n");
+    strcpy(snd, "wifi.setmode(wifi.STATION)\r\n");
+    SendCMD();
+    timeout=4;
+    getreply();
+    pc.printf(buf);
+
+    wait(2);
+
+
+
+    pc.printf("\n---------- Listing Access Points ----------\r\n");
+    strcpy(snd, "function listap(t)\r\n");
+    SendCMD();
+    wait(0.2);
+    strcpy(snd, "for k,v in pairs(t) do\r\n");
+    SendCMD();
+    wait(0.2);
+    strcpy(snd, "print(k..\" : \"..v)\r\n");
+    SendCMD();
+    wait(0.2);
+    strcpy(snd, "end\r\n");
+    SendCMD();
+    wait(0.2);
+    strcpy(snd, "end\r\n");
+    SendCMD();
+    wait(0.2);
+    strcpy(snd, "wifi.sta.getap(listap)\r\n");
+    SendCMD();
+    wait(0.2);
+    timeout=15;
+    getreply();
+    pc.printf(buf);
+
+    wait(0.5);
+
+    pc.printf("\n---------- Connecting to AP ----------\r\n");
+    pc.printf("ssid = %s   pwd = %s\r\n",ssid,pwd);
+    strcpy(snd, "wifi.sta.config(\"");
+    strcat(snd, ssid);
+    strcat(snd, "\",\"");
+    strcat(snd, pwd);
+    strcat(snd, "\")\r\n");
+    SendCMD();
+    timeout=10;
+    getreply();
+    pc.printf(buf);
+
+    wait(2);
+
+    pc.printf("\n---------- Get IP's ----------\r\n");
+    strcpy(snd, "print(wifi.sta.getip())\r\n");
+    SendCMD();
+    timeout=1;
+    getreply();
+    pc.printf(buf);
+
+    wait(1);
+
+    pc.printf("\n---------- Get Connection Status ----------\r\n");
+    strcpy(snd, "print(wifi.sta.status())\r\n");
+    SendCMD();
+    timeout=5;
+    getreply();
+    pc.printf(buf);
+
+    pc.printf("\n\n\n  If you get a valid (non zero) IP, ESP8266 has been set up.\r\n");
+    pc.printf("  Run this if you want to reconfig the ESP8266 at any time.\r\n");
+    pc.printf("  It saves the SSID and password settings internally\r\n");
+    wait(1);
+
+    timeout=5;
+
+    pc.printf("Starting server creation...\n");
+    //create server
+    strcpy(snd, "srv=net.createServer(net.TCP)\r\n");
+    SendCMD();
+    getreply();
+    wait(1);
+    strcpy(snd,"srv:listen(80,function(conn)\r\n");
+    SendCMD();
+    getreply();
+    wait(1);
+    strcpy(snd,"conn:on(\"receive\",function(conn,payload) \r\n");
+    SendCMD();
+    getreply();
+    wait(1);
+
+    //print data to mbed
+    strcpy(snd,"print(payload)\r\n");
+    SendCMD();
+    getreply();
+    wait(1);
+
+
+    //Create HTML
+    strcpy(snd, "conn:send('<!DOCTYPE html><html><body><h1>Mbed Music Device Controls</h1>')\r\n");
+    SendCMD();
+    getreply();
+    wait(1);
+
+    strcpy(snd,"conn:send('<form method=\"POST\">')\r\n");
+    SendCMD();
+    getreply();
+    wait(1);
+
+        strcpy(snd,"conn:send('<p><input type=\"radio\" id=\"start\" name=\"action\" value=\"START\"></p>')\r\n");
+        SendCMD();
+        getreply();
+        wait(1);
+        strcpy(snd,"conn:send('<p><label for=\"start\">Start</label><br></p>')\r\n");
+        SendCMD();
+        getreply();
+        wait(1);
+        strcpy(snd,"conn:send('<p><input type=\"radio\" id=\"stop\" name=\"action\" value=\"STOP\"></p>')\r\n");
+        SendCMD();
+        getreply();
+        wait(1);
+        strcpy(snd,"conn:send('<p><label for=\"stop\">Stop</label><br></p>')\r\n");
+        SendCMD();
+        getreply();
+        wait(1);
+        strcpy(snd,"conn:send('<p><label for=\"vol\">Volume (0-100):</label></p>')\r\n");
+        SendCMD();
+        getreply();
+        wait(1);
+        strcpy(snd,"conn:send('<p><input type=\"range\" id=\"vol\" name=\"vol\" min=\"0\" max=\"100\"></p>')\r\n");
+        SendCMD();
+        getreply();
+        wait(1);
+        strcpy(snd,"conn:send('<p><input type=\"submit\" value=\"Refresh\"></p></form>')\r\n");
+        SendCMD();
+        getreply();
+        wait(1);
+    strcpy(snd,"conn:send('<p><h2>How to use:</h2><ul><li>Click Start to start</li><li>Click Stop to stop</li><li>Click Refresh to update volume data</li></ul></body></html>')\r\n");
+    SendCMD();
+    getreply();
+    wait(1);
+    // end web page data
+    strcpy(snd, "conn:on(\"sent\",function(conn) conn:close() end)\r\n"); // close current connection
+    SendCMD();
+    getreply();
+    wait(1);
+    strcpy(snd, "end)\r\n");
+    SendCMD();
+    getreply();
+    wait(1);
+    strcpy(snd, "end)\r\n");
+    SendCMD();
+    getreply();
+    wait(1);
+    /*
+    strcpy(snd,"end)\r\n");
+    SendCMD();
+    getreply();
+    wait(1);
+    */  
+    pc.printf("\n\n++++++++++ Ready ++++++++++\r\n\n");
+}
+
+void SendCMD()
+{
+    esp.printf("%s", snd);
+}
+ 
+void getreply()
+{
+    memset(buf, '\0', sizeof(buf));
+    t.start();
+    ended=0;
+    bytesRead=0;
+    while(!ended) {
+        if(esp.readable() > 0) {
+            buf[bytesRead] = esp.getc();
+            bytesRead++;
+        }
+        if(t.read() > timeout) {
+            ended = 1;
+            t.stop();
+            t.reset();
+        }
+    }
+    pc.printf("From ESP: %s\r\n", buf);
+}
+
+void readWebData(){
+    memset(buf, '\0', sizeof(buf));
+    //pc.printf("ESP readable?: %d bytesReader: %d\r\n", esp.readable(), ++bytesReader);
+    timeout = 3;
+    if (esp.readable() > 0){
+        t.start();
+        ended=0;
+        bytesRead=0;
+        while(!ended) {
+            if(esp.readable() > 0) {
+                buf[bytesRead] = esp.getc();
+                bytesRead++;
+            }
+            if(t.read() > timeout) {
+                ended = 1;
+                t.stop();
+                t.reset();
+            }
+        }   
+        
+        pc.printf("From Webpage: %s\r\n", buf);
+        
+        if( strstr(buf, "action=START") != NULL ) {
+            led1=1;
+            led2=1;
+            led4=1;
+            MUSIC_DEVICE_ENABLED = true;
+            acc.activate();
+        }
+        if( strstr(buf, "action=STOP") != NULL ) {
+            led1=0;
+            led2=0;
+            led3=0;
+            led4=0;
+            red = 0.0;
+            green = 0.0;
+            blue = 0.0;
+            MUSIC_DEVICE_ENABLED = false;
+            speaker = 0.0;
+            acc.standby();
+        }
+        if( strstr(buf, "vol") != NULL ) {
+            num = strstr(buf, "vol");
+            vol[0] = *(num+4);
+            vol[1] = *(num+5);
+            if (*(num+6) == '\r'){
+            vol[2] = '\0';    
+            } else {
+            vol[2] = *(num+6);
+            vol[3] = '\0';   
+            }
+            
+            pc.printf("received volume: %s\r\n", vol);
+            
+            led3 = atof(vol)/100.0;
+            vol_f = atof(vol)/100.0;
+            //Set the volume
+            if(MUSIC_DEVICE_ENABLED){
+                speaker = vol_f;
+            }
+        }
+    }
+    //pc.printf("Finished checking\r\n");
+}
             
 int main() {
-   pb.mode(PullUp);
- 
-   MMA8452 acc(p9, p10, 40000);  //instantiate an acc object! 
-   
-   //set parameters -- use these and don't change
-   acc.setBitDepth(MMA8452::BIT_DEPTH_12);
-   acc.setDynamicRange(MMA8452::DYNAMIC_RANGE_4G);
-   acc.setDataRate(MMA8452::RATE_100);
-   
-   while(1){
-       ind = 0;
-       memset(Song, 0, sizeof(Song)); //reset Song array
-       
-       while(ind < 50) {
-          
-          //uLCD.circle(-1*y*factor+offsety, -1*x*factor+offsetx, radius, BLACK);
-           
-          if(!acc.isXYZReady()) 
-          {
-             wait(0.01);
-          }
-          else
-          { 
-          
-          acc.readXYZGravity(&x,&y,&z); //notice this is passed by reference use pointers
-          
-          setNote(); //find note based on position and show it on led
-          
-          //if button pressed, play note
-          if(!pb){
-            playNote();
-          }
-          else {
-              Note = 'r';
-              speaker = 0.0;
-          }
-          
-          recordNote();
-          
-          wait(0.1); //0.01 was giving a very bad sound!
-          
-          } //end else 
-       } //end infinite while loop
+    pb.mode(PullUp);
 
-       //print final song!
-       speaker = 0.0;
-       red = 1.0;
-       green = 1.0;
-       blue = 1.0;
-       pc.printf("%s\r\n", Song);
-       wait(3);
+    //set parameters -- use these and don't change
+    acc.setBitDepth(MMA8452::BIT_DEPTH_12);
+    acc.setDynamicRange(MMA8452::DYNAMIC_RANGE_2G);
+    acc.setDataRate(MMA8452::RATE_800);
+
+    acc.activate();
+    //Setup webpage
+    pc.baud(9600);  // set what you want here depending on your terminal program speed
+    pc.printf("\f\n\r-------------ESP8266 Hardware Reset-------------\n\r");
+    wait(0.5);
+    reset=1;
+    timeout=2;
+    getreply();
+
+    esp.baud(9600);
+    ESPconfig();   
+   
+    while(1){
+        readWebData();
+        
+        ind = 0;
+        memset(Song, 0, sizeof(Song)); //reset Song array
+
+        while(ind < 50) {
+            if (MUSIC_DEVICE_ENABLED){
+                //uLCD.circle(-1*y*factor+offsety, -1*x*factor+offsetx, radius, BLACK);
+                acc.readXYZGravity(&x,&y,&z); //notice this is passed by reference use pointers
+
+                setNote(); //find note based on position and show it on led
+                
+                //if button pressed, play note
+                if(!pb){
+                    playNote();
+                } else {
+                    Note = 'r';
+                    speaker = 0.0;
+                }
+
+                recordNote();
+                pc.printf("Note, Index, X, isReady, status: %c, %d, %f, %d\r\n", Note, ind, x, acc.isXYZReady());
+
+                //Because checking the webpage may take time, we 
+                //may not want to wait any time at all
+                wait(0.15); //0.01 was giving a very bad sound!
+
+                //check for new webpage data  
+            }
+            readWebData();
+        } //end 50 while 
+
+        acc.standby();
+        //print final song!
+        speaker = 0.0;
+        red = 1.0;
+        green = 1.0;
+        blue = 1.0;
+        pc.printf("%s\r\n", Song);
+        acc.activate();
+        wait(3);
+        
     }
 } // end main
